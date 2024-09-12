a10094097546e250e6b354158f61dab10e55a186
prateekkumary <prateekkumary73@gmail.com>
2024-09-12 14:59:36 +0530

    function map and testcases added

--- items/solution.js
+++ items/solution.js
@@ -7,10 +7,23 @@ function each(elements, cb) {
     // This only needs to work with arrays.
     // You should also pass the index into `cb` as the second argument
     // based off http://underscorejs.org/#each
-    for(let i=0;i<elements.length;i++){
-        cb(elements[i],i)
+    for(let index=0;index<elements.length;index++){
+        cb(elements[index],index)
     }
 }
   
 
-  export{each}
+function map(elements, cb) {
+    // Do NOT use .map, to complete this function.
+    // How map works: Map calls a provided callback function once for each element in an array, in order, and functionructs a new array from the res .
+    // Produces a new array of values by mapping each value in list through a transformation function (iteratee).
+    // Return the new array.
+
+    const result=[];
+    for(let index=0;index<elements.length;index++){
+        result.push(cb(elements[index],index));
+    }
+  }
+  
+
+  export{each,map}
--- items/test.js
+++ items/test.js
@@ -2,10 +2,15 @@
 
 import { items } from "./arrays.js";
 
-import { each } from "./solution.js";
+import { each ,map} from "./solution.js";
 
 
 
 each(items,(element,index)=>{
     console.log(`Element: ${element}, Index: ${index}`);
 })
+
+
+
+ map(items,(element)=> console.log(element*element));
+
