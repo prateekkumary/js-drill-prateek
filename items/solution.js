02f7fc6266fb8ee3bf04223f9b633fff49081049
prateekkumary <prateekkumary73@gmail.com>
2024-09-12 17:24:24 +0530

    function reduce added

--- items/solution.js
+++ items/solution.js
@@ -24,6 +24,35 @@ function map(elements, cb) {
         result.push(cb(elements[index],index));
     }
   }
+
+  function reduce(elements, cb, startingValue) {
+    // Do NOT use .reduce to complete this function.
+    // How reduce works: A reduce function combines all elements into a single value going from left to right.
+    // Elements will be passed one by one into `cb` along with the `startingValue`.
+    // `startingValue` should be the first argument passed to `cb` and the array element should be the second argument.
+    // `startingValue` is the starting value.  If `startingValue` is undefined then make `elements[0]` the initial value.
+
+    let acc;
+    let startIndex;
+
+    
+    if (startingValue !== undefined) {
+        acc = startingValue;
+        startIndex = 0; // Start from the first element
+    } else {
+        acc = elements[0];
+        startIndex = 1; // Start from the second element since the first is used as the initial value
+    }
+
+    // Iterate through the array, applying the callback and updating the accumulator
+    for (let i = startIndex; i < elements.length; i++) {
+        acc = cb(acc, elements[i]);
+    }
+
+    // Return the accumulated result
+    return acc;
+  }
+  
   
 
-  export{each,map}
+  export{each,map,reduce}
--- items/test.js
+++ items/test.js
@@ -2,15 +2,20 @@
 
 import { items } from "./arrays.js";
 
-import { each ,map} from "./solution.js";
+import { each ,map,reduce} from "./solution.js";
 
 
-
+// #1
 each(items,(element,index)=>{
     console.log(`Element: ${element}, Index: ${index}`);
 })
 
 
+//#2
+map(items,(element)=> console.log(element*element));
 
- map(items,(element)=> console.log(element*element));
+//#3
 
+console.log(reduce(items, (acc, element) => {
+    return acc *= element;
+}, 1));
